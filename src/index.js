ea3375adaa39f95870c7c427a19025dbaf3d98fc
LeoLin <leo.lin@run-cloud.com.tw>
2022-01-18 16:39:48 +0800

    Initialize project using Create React App

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReaderPage from './pages/ReaderPage';
+
+import './global.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <ReaderPage />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
---
+++ src/pages/ReaderPage.jsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const NormalDiv = styled.div`
+  background-color: #f0f0f0;
+  padding: 20px;
+`
+
+const ReaderPage = () => (
+  <NormalDiv>
+    <h1>Reader Page</h1>
+  </NormalDiv>
+)
+
+export default ReaderPage
